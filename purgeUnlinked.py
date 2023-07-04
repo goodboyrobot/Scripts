dc445512487acf0f0e3c17a0f9e3a5c0f7d0d8db
Alex Schuldberg <afschuld@gmail.com>
2023-07-04 15:12:34 -0700

    updating purge unlinked script

--- purgeUnlinked.py
+++ purgeUnlinked.py
@@ -4,15 +4,19 @@ import subprocess
 
 for file in os.listdir(sys.argv[1]):
 	#build up args array
-	cmd = [0,0,0]
-	cmd[0] = "stat" 
-	cmd[1] = "--printf=\'%h\'"
-	cmd[2] = sys.argv[1] + file
-	#print cmd
-	numLinks = subprocess.check_output(cmd)
-	#print numLinks
-	if (numLinks == '\'1\''):
-		print("deleting " + file)
-		delcmd = "rm " + cmd[2]
-		#print delcmd
-		os.system(delcmd)
+    fullpath = sys.argv[1] + '/' + file
+    if os.path.isfile(fullpath):
+        cmd = [0,0,0]
+        cmd[0] = "stat" 
+        cmd[1] = "--printf=\'%h\'"
+        cmd[2] = fullpath
+        #print(cmd)
+        numLinks = subprocess.check_output(cmd).decode('utf-8')
+        print(numLinks)
+        if (numLinks == '\'1\''):
+            print("deleting " + file)
+            delcmd = "rm " + fullpath
+            print(delcmd)
+            os.system(delcmd)
+        
+    
